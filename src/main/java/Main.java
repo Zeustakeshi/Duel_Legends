291832ef5c80a328c9a5c22823564104bd69d45a
Minh Hiếu <gaconmvp2312@gmail.com>
2024-06-14 00:27:46 +0700

    update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import game.Game;
 public class Main {
     public static void main(String[] args) {
         System.out.println("test main v2");
+        System.out.println("123213123");
         new Game();
     }
 }
